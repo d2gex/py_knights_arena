fb6df0683c09ef33fda00ef7639f7a8a45b2c3ee
d2gex <daniel.garcia@d2garcia.com>
2019-10-07 08:44:32 +0100

    Add Knights and Item classes

---
+++ src/cell_content.py
@@ -0,0 +1,122 @@
+class CellContent:
+
+    def __init__(self, name, nickname, status, attack_score, defence_score):
+        self.name = name
+        self.nickname = nickname
+        self._attack_score = attack_score
+        self._defence_score = defence_score
+        self._status = status
+
+    @property
+    def attack_score(self):
+        return self._attack_score
+
+    @property
+    def defence_score(self):
+        return self._defence_score
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, new_status):
+        self._status = new_status
+
+    def __str__(self):
+        return self.nickname
+
+
+class Item(CellContent):
+
+    def __gt__(self, other):
+        '''An item is more powerful than another one if this has either more attacking power or having the same
+        it has more defence power.
+        '''
+        if self.attack_score > other.attack_score:
+            return True
+        if self.attack_score < other.attack_score:
+            return False
+        return self.defence_score > other.defence_score
+
+    def __ge__(self, other):
+        if self.attack_score + self.defence_score - other.attack_score + other.defence_score == 0:
+            return True
+        return self.__gt__(other)
+
+
+class Knight(CellContent):
+
+    def __init__(self,*args, surprise=0.5):
+        super().__init__(*args)
+        self.surprise = surprise
+        self._item = None
+
+    @property
+    def item(self):
+        return self._item
+
+    @item.setter
+    def item(self, new_item):
+        self._item = new_item
+
+    def pick_item(self, items):
+        '''A knight will pick the most powerful item available when given a few of them based on
+        '''
+        if isinstance(items, Item):
+            if not self.item:
+                self._item = items
+        else:
+            self._item = sorted(items + [self.item] if self.item else items, reverse=True)[0]
+
+    def beat(self, knight):
+        '''At knight beats another knight if its attacking power is bigger than its rival defensive one
+        '''
+        return (self.surprise + self.attack_score + self.item.attack_score) > \
+               (knight.defence_score + knight.item.defence_score)
+
+
+class ItemFactory:
+
+    @classmethod
+    def axe(cls):
+        return Item('Axe', 'A', status=False, attack_score=2, defence_score=0)
+
+    @classmethod
+    def magic_staff(cls):
+        return Item('MagicStaff', 'M', status=False, attack_score=1, defence_score=1)
+
+    @classmethod
+    def dagger(cls):
+        return Item('Dagger', 'D', status=False, attack_score=1, defence_score=0)
+
+    @classmethod
+    def helmet(cls):
+        return Item('Helmet', 'H', status=False, attack_score=0, defence_score=1)
+
+    @classmethod
+    def all(cls):
+        return cls.axe(), cls.magic_staff(), cls.dagger(), cls.helmet()
+
+
+class KnightFactory:
+
+    @classmethod
+    def red(cls):
+        return Knight('RED', 'R', 'LIVE', 1, 1)
+
+    @classmethod
+    def green(cls):
+        return Knight('GREEN', 'G', 'LIVE', 1, 1)
+
+    @classmethod
+    def blue(cls):
+        return Knight('BLUE', 'B', 'LIVE', 1, 1)
+
+    @classmethod
+    def yellow(cls):
+        return Knight('YELLOW', 'Y', 'LIVE', 1, 1)
+
+    @classmethod
+    def all(cls):
+        return cls.red(), cls.green(), cls.blue(), cls.yellow()
---
+++ tests/test_cell_content.py
@@ -0,0 +1,133 @@
+import pytest
+from src.cell_content import Item, Knight, ItemFactory, KnightFactory
+
+
+@pytest.fixture(scope='module')
+def items():
+    return ItemFactory.all()
+
+
+@pytest.fixture(scope='module')
+def knights(items):
+    axe, magic_staff, dagger, helmet = items
+    kn1, kn2, kn3, kn4 = KnightFactory.all()
+    kn1.item, kn2.item, kn3.item, kn4.item = axe, magic_staff, dagger, helmet
+    return kn1, kn2, kn3, kn4
+
+
+def test_item_power(items):
+    axe, magic_staff, dagger, helmet = items
+    # axe is por powerful than anything else
+    assert all(axe > item for item in(magic_staff, dagger, helmet))
+
+    # magi_staff is the second most powerful
+    assert all(magic_staff > item for item in(dagger, helmet))
+
+    # dagger is more powerful than helmet
+    assert dagger > helmet
+
+    # Those items that are the same are the same powerful
+    assert axe >= Item('Axe', 'A', status=False, attack_score=2, defence_score=0)
+
+
+class TestKnights:
+    '''knights on attack:   (surprise + base + attack)
+    a) kn1(axe) =>          (0.5 + 1 + 2) = 3.5
+    b) kn2(magic_staff) =>  (0.5 + 1 + 1) = 2.5
+    c) kn3(dagger) =>       (0.5 + 1 + 1) = 2.5
+    d) kn4(helmet) =>       (0.5 + 1 + 0) = 1.5
+
+    knights on defence: (base + defence)
+    e) kn1(axe) =>          (1 + 0) = 1
+    f) kn2(magic_staff) =>  (1 + 1) = 2
+    g) kn3(dagger) =>       (1 + 0) = 1
+    h) kn4(helmet) =>       (1 + 1) = 2
+    '''
+
+    def test_knight_with_axe_attack(self, knights):
+        '''On attack knight with axe beats everybody
+        '''
+        kn1, kn2, kn3, kn4 = knights
+        assert all(kn1.beat(knight) for knight in (kn2, kn3, kn4))
+
+    def test_knight_with_axe_defence(self, knights):
+        '''On defence knight with axe is beaten by everybody
+        '''
+        kn1, kn2, kn3, kn4 = knights
+        assert all(knight.beat(kn1) for knight in (kn2, kn3, kn4))
+
+    def test_knight_with_magic_staff_attack(self, knights):
+        '''On attack knight with magic_staff beats everybody
+        '''
+        kn1, kn2, kn3, kn4 = knights
+        assert all(kn2.beat(knight) for knight in (kn1, kn3, kn4))
+
+    def test_knight_with_magic_staff_defence(self, knights):
+        '''On defence knight with magic_staff is only beaten by those either with axe or dagger
+        '''
+        kn1, kn2, kn3, kn4 = knights
+        assert not kn4.beat(kn2)
+        assert all(knight.beat(kn2) for knight in (kn1, kn3))
+
+    def test_knight_with_dagger_attack(self, knights):
+        '''On attack knight with dagger beats everybody
+        '''
+        kn1, kn2, kn3, kn4 = knights
+        assert all(kn3.beat(knight) for knight in (kn1, kn2, kn4))
+
+    def test_knight_with_dagger_defence(self, knights):
+        '''On defence knight with dagger is beaten by everybody
+        '''
+        kn1, kn2, kn3, kn4 = knights
+        assert all(knight.beat(kn3) for knight in (kn1, kn2, kn4))
+
+    def test_knight_with_helmet_attack(self, knights):
+        '''On attack knight with helmet only beats axe and dagger
+        '''
+        kn1, kn2, kn3, kn4 = knights
+        assert not kn4.beat(kn2)
+        assert all(kn4.beat(knight) for knight in (kn1, kn3))
+
+    def test_knight_with_helmet_defence(self, knights):
+        '''On defence knight with helmet is beaten by everybody
+        '''
+        kn1, kn2, kn3, kn4 = knights
+        assert all(knight.beat(kn4) for knight in (kn1, kn2, kn3))
+
+    def test_pick_items(self):
+        '''A knight will:
+        a) pick an item if none
+        b) ignore item if one already assigned
+        c) pick the best item of various, including the one given following an order of [A,M,D,H]
+        '''
+
+        # a)
+        red = KnightFactory.red()
+        axe = ItemFactory.axe()
+        assert not red.item
+        red.pick_item(axe)
+        assert red.item == axe
+
+        # b)
+        magic_staff = ItemFactory.magic_staff()
+        red.pick_item(magic_staff)
+        assert red.item == axe
+
+        # c)
+        red.item = None
+        helmet = ItemFactory.helmet()
+        red.pick_item(helmet)
+        assert red.item == helmet
+        red.pick_item([magic_staff, axe])
+        assert red.item == axe
+
+        # c.1
+        # --> if the most powerful is the one held ignore the rest
+        red.pick_item([magic_staff, helmet])
+        assert red.item == axe
+
+        # c.2
+        dagger = ItemFactory.dagger()
+        red.item = dagger
+        red.pick_item([magic_staff, helmet])
+        assert red.item == magic_staff
